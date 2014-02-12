f98dfa5db8acb236ceae3a23d7d5b44b7950ec22
Patryk Połomski <patryk@ephp.pl>
2014-02-12 22:46:17 +0100

    fiirst commit

---
+++ manifests/default.pp
@@ -0,0 +1,128 @@
+## Configuration START
+
+$db_name = "symfony" # production env db name, should not be longer than 12 characters
+
+## Configuration END
+
+group { 'puppet': ensure => present }
+Exec { path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ] }
+File { owner => 0, group => 0, mode => 0644 }
+
+file { "/var/lock/apache2":
+  ensure => directory,
+  owner => vagrant
+}
+
+exec { "ApacheUserChange" :
+  command => "sed -i 's/export APACHE_RUN_USER=.*/export APACHE_RUN_USER=vagrant/ ; s/export APACHE_RUN_GROUP=.*/export APACHE_RUN_GROUP=vagrant/' /etc/apache2/envvars",
+  require => [ Package["apache"], File["/var/lock/apache2"] ],
+  notify  => Service['apache'],
+}
+
+
+class {'apt':
+  always_apt_update => true,
+}
+
+Class['::apt::update'] -> Package <|
+    title != 'python-software-properties'
+and title != 'software-properties-common'
+|>
+
+    apt::key { '4F4EA0AAE5267A6C': }
+
+apt::ppa { 'ppa:ondrej/php5-oldstable':
+  require => Apt::Key['4F4EA0AAE5267A6C']
+}
+
+package { [
+    'build-essential',
+    'vim',
+    'curl',
+    'git-core',
+    'mc'
+  ]:
+  ensure  => 'installed',
+}
+
+class { 'apache': }
+
+apache::dotconf { 'custom':
+  content => 'EnableSendfile Off',
+}
+
+apache::module { 'rewrite': }
+
+apache::vhost { "default":
+  server_name   => "default",
+  serveraliases => ["*"],
+  docroot       => "/var/www/web/",
+  port          => '80',
+  env_variables => [
+],
+  priority      => '1',
+}
+
+class { 'php':
+  service             => 'apache',
+  service_autorestart => false,
+  module_prefix       => '',
+}
+
+php::module { 'php5-mysql': }
+php::module { 'php5-sqlite': }
+php::module { 'php5-cli': }
+php::module { 'php5-curl': }
+php::module { 'php5-intl': }
+php::module { 'php5-mcrypt': }
+php::module { 'php5-gd': }
+php::module { 'php-apc': }
+php::pecl::module { 'xdebug': }
+
+class { 'php::devel':
+  require => Class['php'],
+}
+
+class { 'php::pear':
+  require => Class['php'],
+}
+
+php::pear::module { 'PHPUnit':
+  repository  => 'pear.phpunit.de',
+  use_package => 'no',
+  require => Class['php::pear']
+}
+
+php::ini { 'php_ini_configuration':
+  value   => [
+    'date.timezone = "Europe/Warsaw"',
+    'display_errors = On',
+    'error_reporting = -1',
+    'short_open_tag = 0',
+    'xdebug.idekey = vagrant_debug',
+    'xdebug.default_enable = 1',
+    'xdebug.remote_autostart = 1',
+    'xdebug.remote_connect_back = 1',
+    'xdebug.remote_enable = 1',
+    'xdebug.remote_handler = "dbgp"',
+    'xdebug.remote_port = 9000',
+    'xdebug.max_nesting_level = 1000'
+  ],
+  notify  => Service['apache'],
+  require => Class['php']
+}
+
+class { 'mysql::server':
+  override_options => { 'root_password' => '', }
+}
+
+mysql::db { "${db_name}":
+  grant    => [
+    'ALL'
+  ],
+  user     => "${db_name}",
+  password => "${db_name}",
+  host     => 'localhost',
+  charset  => 'utf8',
+  require  => Class['mysql::server'],
+}
